43915cac782946ac285d64453ac169e0d1f39fea
Mridul Dhiman <dhimanmridul91@gmail.com>
2023-09-01 23:42:10 +0530

    added HEADINGS

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -12,6 +12,25 @@ const actualContent = content.split("\n");
     <h3>Hello from Preview</h3>
 {
     actualContent.map((line) => {
+        console.log(line);
+        if(line.startsWith("# ")){
+            return <h1>{line.slice(2)}</h1>
+        }
+        else if(line.startsWith("## ")) {
+            return <h2>{line.slice(3)}</h2>
+        }
+        else if(line.startsWith("### ")){
+            return <h3>{line.slice(4)}</h3>
+        }
+        else if(line.startsWith("#### ")) {
+            return <h4>{line.slice(5)}</h4>
+        }
+        else if(line.startsWith("##### ")) {
+            return <h5>{line.slice(6)}</h5>
+        }
+        else if(line.startsWith("###### ")) {
+            return <h6>{line.slice(7)}</h6>
+        }
         return <p>{line}</p>
     })
 }
