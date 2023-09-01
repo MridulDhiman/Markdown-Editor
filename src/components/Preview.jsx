447ac314ced8c492992c5c00f8d069e13f5e0fe3
Mridul Dhiman <dhimanmridul91@gmail.com>
2023-09-02 00:15:05 +0530

    added horizontal rules tag, as well as appended to the headings

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -12,24 +12,49 @@ const actualContent = content.split("\n");
     <h3>Hello from Preview</h3>
 {
     actualContent.map((line) => {
-        console.log(line);
+        if(line.trim() === "---") {
+            console.log("its working");
+              return <hr/>
+        }
+
+
         if(line.startsWith("# ")){
-            return <h1>{line.slice(2)}</h1>
+            return <>
+             <h1>{line.slice(1)}</h1>
+             <hr/>
+            </>
+           
         }
         else if(line.startsWith("## ")) {
-            return <h2>{line.slice(3)}</h2>
+            return  <>
+             <h2>{line.slice(2)}</h2>
+             <hr/>
+            </>
+            
         }
         else if(line.startsWith("### ")){
-            return <h3>{line.slice(4)}</h3>
+            return  <>
+             <h3>{line.slice(3)}</h3>
+             <hr/>
+            </>
         }
         else if(line.startsWith("#### ")) {
-            return <h4>{line.slice(5)}</h4>
+            return  <>
+             <h4>{line.slice(4)}</h4>
+             <hr/>
+            </>
         }
         else if(line.startsWith("##### ")) {
-            return <h5>{line.slice(6)}</h5>
+            return  <>
+             <h5>{line.slice(5)}</h5>
+             <hr/>
+            </>
         }
         else if(line.startsWith("###### ")) {
-            return <h6>{line.slice(7)}</h6>
+            return  <>
+             <h6>{line.slice(6)}</h6>
+             <hr/>
+            </>
         }
         if(line.startsWith("**") && line.endsWith("**") ) {
            return (
