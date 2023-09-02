7d1abfa79c6e1f00606f16e7c059c3d40b287fbe
Mridul Dhiman <dhimanmridul91@gmail.com>
2023-09-03 04:40:36 +0530

    1. and nth ordered list added

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -8,9 +8,6 @@ import {marked} from "marked";
 const Preview = ({content})=> {
 const actualContent = content.split("\n");
 
-
-
-
     return (
 <div  className={`${styles["preview"]} ${UIstyles['container']}`}>
     <h3>Hello from Preview</h3>
@@ -19,7 +16,28 @@ const actualContent = content.split("\n");
          const x = (marked.parse(line.trim()));
          
          const regex = /^<ul>/;
-         console.log(x.match(regex));
+         const regex2 = /^<ol/;
+         const regex3 = /<li>([^<]+)<\/li>/;
+         const regex4 = /="([^"]+)"/;
+
+       
+        // // console.log(line.trim(), x);
+        // console.log(regex4.exec(x));
+        // console.log(regex3.exec(x));
+
+         if(x.match(regex2)) {
+            return(
+                <>
+                {!regex4.exec(x) ? 
+                <div dangerouslySetInnerHTML={{__html:  regex3.exec(x) && `1. ${regex3.exec(x)[1]}`}}></div> : 
+                <div dangerouslySetInnerHTML={{__html:  regex3.exec(x) && `${regex4.exec(x)[1]}. ${regex3.exec(x)[1]}`}} ></div>
+                }
+                </>
+            ) 
+         }
+
+         
+       
         return( <div dangerouslySetInnerHTML={{__html:  (line.trim().startsWith("* ") && x.match(regex) )?  x.match(regex)?.input.slice(5, -7) : x}}></div>)
     })
 }
