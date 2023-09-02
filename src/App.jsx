feac037add050338e8715d853b3bb5168bc196d9
Mridul Dhiman <dhimanmridul91@gmail.com>
2023-09-02 23:36:00 +0530

    unordered list initial version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Markdown from "./components/Markdown"
 import Preview from "./components/Preview"
 import Header from "./components/Header"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 // import Header from "./components/Header"
 
 import styles from "./App.module.css";
@@ -9,17 +9,25 @@ import "./main.css";
 
 function App() {
 const [content, setContent] = useState("");
+const [isEntered, setIsEntered] = useState(false);
+
 
 const handleContentChange = (data) => {
   const x = data.split(/\r?\n/).join('\n');
   setContent(x);
 }
+
+const handleEnter = (enterClicked) => {
+if(enterClicked) {
+  setIsEntered(true);
+}
+}
   return (
     <div className={styles["whole"]}>
     <Header/>
     <div className={styles["main"]}>
-    <Markdown onContentChange={handleContentChange}/> 
-    <Preview content={content}/>
+    <Markdown onContentChange={handleContentChange}  onEnter={handleEnter}/> 
+    <Preview content={content} isEntered={isEntered} />
     </div>
      </div>
   )
--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -3,14 +3,20 @@ import React from "react";
 import styles from "./Markdown.module.css";
 import UIstyles  from "./Container.module.css";
 
-const Markdown = ({onContentChange})=> {
+const Markdown = ({onContentChange,onEnter})=> {
     const handleChange = (e)=> {
         onContentChange(e.target.value);  
+    }
+
+    const handleKeyDown = (e)=> {
+        if(e.key === "Enter") {
+                onEnter(true);
+        }
     }
     return (
 <div className={`${styles["markdown"]} ${UIstyles['container']}`}>
     <h3>Hello From Markdown</h3>
-    <textarea name="markdown" className={styles['markdown-input']} onChange={handleChange}></textarea>
+    <textarea name="markdown" className={styles['markdown-input']} onChange={handleChange} onKeyDown={handleKeyDown}></textarea>
 </div>
     )
 }
